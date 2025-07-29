cf29b9f08f4d36716d0aeb6ad38c7673a340e0e0
Gabriella Franco <gabriella-nena@hotmail.com>
2025-07-29 16:36:29 -0300

    Commit inicial

---
+++ src/main/java/com/enterprise/ong_pet/OngPetApplication.java
@@ -0,0 +1,13 @@
+package com.enterprise.ong_pet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OngPetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OngPetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enterprise/ong_pet/OngPetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enterprise.ong_pet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OngPetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
